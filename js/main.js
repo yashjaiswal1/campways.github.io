2b5f4f8f8d9b325e8bc92d2595c716d0fbdcb002
yashjaiswal1 <yashjaiswal1@outlook.com>
2019-08-08 22:50:12 +0530

    testing.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 window.onload = () => {
-    'use strict';
-  
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker
-               .register('../sw.js');
-    }
+  'use strict';
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+             .register('./sw.js');
   }
+}
